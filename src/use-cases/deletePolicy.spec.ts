3b3dbfa7e398c724ba7e5a793920db50045edf4c
Matheus Sanchez <matheus_sanchez@usp.br>
2023-12-16 18:51:09 -0300

    delete policy Test

---
+++ src/use-cases/deletePolicy.spec.ts
@@ -0,0 +1,64 @@
+import { expect, beforeEach, describe, it } from 'vitest'
+import { PolicyRepository } from '../repositories/policyRepository'
+import { InMemomryPolicyRepository } from '../repositories/in-memory-db/inMemoryPolicyRepository'
+import { DeletePolicyUseCase } from './deletePolicy'
+import { ResourceNotFoundError } from './errors/resourceNotFound'
+
+let policyRepository: PolicyRepository
+let deletePolicyUseCase: DeletePolicyUseCase
+
+describe('Delete Policy Use Case', () => {
+  beforeEach(async () => {
+    policyRepository = new InMemomryPolicyRepository()
+    deletePolicyUseCase = new DeletePolicyUseCase(policyRepository)
+  })
+
+  it('should be able to delete a Policy', async () => {
+    await policyRepository.create({
+      policyName: 'policyTestToDelete',
+      comparators: [
+        {
+          id: 1,
+          type: 'result',
+          result: false,
+        },
+        {
+          id: 2,
+          type: 'result',
+          result: true,
+        },
+      ],
+    })
+
+    const deletedPolicy = await deletePolicyUseCase.execute({
+      policyName: 'policyTestToDelete',
+    })
+
+    expect(deletedPolicy.policyName).toEqual('policyTestToDelete')
+    expect(deletedPolicy.comparators).toHaveLength(2)
+  })
+
+  it('should not be able to delete a Policy that do not exist', async () => {
+    await policyRepository.create({
+      policyName: 'wrongPolicy',
+      comparators: [
+        {
+          id: 1,
+          type: 'result',
+          result: false,
+        },
+        {
+          id: 2,
+          type: 'result',
+          result: true,
+        },
+      ],
+    })
+
+    await expect(() =>
+      deletePolicyUseCase.execute({
+        policyName: 'policyThatDoNotExist',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
